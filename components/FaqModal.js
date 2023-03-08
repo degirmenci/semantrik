71d8ecd6e49fb5746f07d1c55a0d5baa23d7fed1
Soysal Degirmenci <degirmenci@MacBook-Pro.local>
2023-03-07 18:18:44 -0800

    Faq update

--- components/FaqModal.js
+++ components/FaqModal.js
@@ -10,9 +10,9 @@ export const FaqModal = ({ isOpen, handleClose }) => {
         </p>
         <p className="text-left text-md text-gray-600 dark:text-gray-200">
             Semantrik her gün hedef kelimenin değiştiği bir bilmece oyunu. Tahmin ettiğiniz her kelimenin
-            hedef kelimeye semantik uzaklığı hesaplanır ve bu sayede bulmacaya ne kadar yakın olduğunu görebilirsiniz. Bu
-            uzaklık kelimelerin cümlelerde ne kadar sıklıkta birlikte kullanıldığına göre hesaplanmaktadır. Örneğin, "kedi"
-            ve "köpek" arasındaki semantik uzaklık, "kedi" ve "araba" arasındaki uzaklıktan daha yüksektir.
+            hedef kelimeye semantik benzerlik skoru hesaplanır ve bu sayede bulmacaya ne kadar yakın olduğunu görebilirsiniz. Bu
+            skor kelimelerin cümleler ve dökümanlarda ne kadar sıklıkta birlikte kullanıldığına göre hesaplanmaktadır. Örneğin, "araba"
+            ve "yarış" arasındaki semantik benzerlik skoru, "araba" ve "kedı" arasındaki benzerlik skorundan daha yüksektir.
         </p>
 
         <p className="text-left py-2 font-bold text-md text-gray-900 dark:text-gray-200">
@@ -21,7 +21,7 @@ export const FaqModal = ({ isOpen, handleClose }) => {
         <p className="text-left text-md text-gray-600 dark:text-gray-200">
             Açık kaynak CC-100 (Common Crawl) Türkçe veri seti kullanılarak, word2vec dil modeli eğitildi. Bu model, her
             kelime için bir vektör oluşturdu ve bu vektörler arasındaki uzaklık kullanılarak 0 ile 100 arasında bir
-            benzerlik skoru hesaplandı. Yüksek benzerlik skoru, kelimenin hedef kelimeye yakın olduğunu gösterir.
+            benzerlik skoru hesaplandı.
         </p>
 
         <p className="text-left py-2 font-bold text-md text-gray-900 dark:text-gray-200">
